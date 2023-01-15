2c7cd8273a7155288c894d7a65fa1be121004134
Vincent Deng <vincentdeng.it@gmail.com>
2023-01-16 00:20:42 +1100

    change back to ngrok

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const default_organization_repository = process.env.DEFAULT_ORGANIZATION_REPOSITORY ?? '.github'
 const app_route = process.env.APP_ROUTE ?? '/org-workflows'
-const ngrok = 'https://veracode.vincentdeng.net'
+const ngrok = 'https://c3f5-13-238-167-251.ngrok.io'
 const config_keys = ['workflows_repository']
 const github_host = process.env.GITHUB_HOST ?? 'https://github.com'
 const artifact_folder = process.env.Artifact_Folder ?? '/Users/vdeng/coding-projects/veracode-github-app-repos/artifacts'
