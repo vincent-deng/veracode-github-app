4a9c0fd8f4c242bab5a00b0128d6ed9e26c193f2
Vincent Deng <vincentdeng.it@gmail.com>
2023-02-27 17:23:54 +1100

    change to production url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const default_organization_repository = process.env.DEFAULT_ORGANIZATION_REPOSITORY ?? 'veracode'
 const app_route = process.env.APP_ROUTE ?? '/org-workflows'
-const ngrok = 'https://e99a-114-77-60-74.au.ngrok.io'
+const ngrok = 'https://veracode.vincentdeng.net'
 const config_keys = ['workflows_repository']
 const github_host = process.env.GITHUB_HOST ?? 'https://github.com'
 const artifact_folder = process.env.Artifact_Folder ?? '/tmp/veracode'
